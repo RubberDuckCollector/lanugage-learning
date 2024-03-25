afc21adb0d92e30b68e921340ca3512f20579dc7
Luna Long <eggsim49@gmail.com>
2024-03-25 20:39:34 +0000

    idk

---
+++ personal/programming/rust/fizzbuzz/target/debug/deps/fizzbuzz-30a26d6458e14352.d
@@ -0,0 +1,5 @@
+/Users/luna/subject-resources/personal/programming/rust/fizzbuzz/target/debug/deps/fizzbuzz-30a26d6458e14352: src/main.rs
+
+/Users/luna/subject-resources/personal/programming/rust/fizzbuzz/target/debug/deps/fizzbuzz-30a26d6458e14352.d: src/main.rs
+
+src/main.rs:
---
+++ personal/programming/rust/fizzbuzz/target/debug/fizzbuzz.d
@@ -0,0 +1,1 @@
+/Users/luna/subject-resources/personal/programming/rust/fizzbuzz/target/debug/fizzbuzz: /Users/luna/subject-resources/personal/programming/rust/fizzbuzz/src/main.rs
