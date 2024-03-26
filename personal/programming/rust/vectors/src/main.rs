d0806d1c9a034db1f3bd8a789091afbb9b5be88b
Luna Long <eggsim49@gmail.com>
2024-03-26 12:54:34 +0000

    rust ignore should be fine now???

--- personal/programming/rust/vectors/src/main.rs
+++ personal/programming/rust/vectors/src/main.rs
@@ -1,11 +1,15 @@
-fn main() {
+// 2024-03-26
+
+fn loop_through_vector() {
     let nums = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+    println!("nums: {:?}", nums);
+
     for i in &nums {
-        println!(
-            "num: {}, index: {}",
-            i,
-            nums.iter().position(|i| i == num).unwrap()
-        );
+        println!("num: {}", *i);
     }
+}
+
+fn main() {
+    loop_through_vector();
 }
