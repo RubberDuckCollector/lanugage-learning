b2650dbdf1bae4427c8fafb656f42e9f550215f4
Luna Long <eggsim49@gmail.com>
2024-03-26 12:27:52 +0000

    cs unit 1

--- personal/programming/rust/fibonacci/target/debug/deps/fibonacci-75eb1f81b753d2f4.d
+++
@@ -1,5 +0,0 @@
-/Users/luna/subject-resources/personal/programming/rust/fibonacci/target/debug/deps/fibonacci-75eb1f81b753d2f4: src/main.rs
-
-/Users/luna/subject-resources/personal/programming/rust/fibonacci/target/debug/deps/fibonacci-75eb1f81b753d2f4.d: src/main.rs
-
-src/main.rs:
--- personal/programming/rust/fibonacci/target/debug/fibonacci.d
+++
@@ -1,1 +0,0 @@
-/Users/luna/subject-resources/personal/programming/rust/fibonacci/target/debug/fibonacci: /Users/luna/subject-resources/personal/programming/rust/fibonacci/src/main.rs
